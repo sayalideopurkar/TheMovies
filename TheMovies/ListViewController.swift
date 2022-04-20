dd17dd91233442b76ea44d1d85664165b1da1286
sayalideopurkar <sayali.deopurkar@gmail.com>
2022-04-20 10:18:32 +0200

    Initial Commit

---
+++ TheMovies/ListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ListViewController.swift
+//  TheMovies
+//
+//  Created by Sayali Deopurkar on 19/04/22.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
